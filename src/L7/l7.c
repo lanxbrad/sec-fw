c144911a9730e61e3096543e2282c88a63e75e6b
royluo <roy.luoye@hotmail.com>
2014-11-28 23:19:33 +0800

    coding

--- src/L7/l7.c
+++ src/L7/l7.c
@@ -7,14 +7,7 @@ void l7_deliver(mbuf_t *m)
 	printf("l7 enter\n");
 #endif
 
-	if(PKTBUF_IS_SW(m))
-		oct_tx_process_mbuf(m, m->input_port);
-		//PACKET_DESTROY_ALL(m);
-	else if(PKTBUF_IS_HW(m))
-		oct_tx_process_mbuf(m, m->input_port);
-	else
-		printf("pkt space error\n");
-	
+	oct_tx_process_mbuf(m, m->input_port);	
 
 	return; 
 }

c3232b39c193320186bae3d83bbd775d3b234c8e
royluo <roy.luoye@hotmail.com>
2014-12-01 11:54:07 +0800

    make a build script

--- start.sh
+++ start.sh
@@ -1,7 +1,64 @@
 #/bin/sh
 
-echo "start....."
-#make clean
+mkdir -p bin
+
+echo "---------------------------------------------------"
+echo "+                                                 +"
+echo "+            Sec Fw Dataplane                     +"
+echo "+                                                 +"
+echo "---------------------------------------------------"
+
+PROG_SECD="secd-linux_64" 
+rm $PROG_SECD
 make OCTEON_TARGET=linux_64
+if [ $? -eq 0 ]; then
+	echo "Dataplane build success!"
+else
+	echo "Error! Dataplane build failed!"
+	exit 
+fi
+
+if [ ! -f "$PROG_SECD" ]; then
+	echo "$PROG_SECD not exist"
+	exit 
+fi
+
+cp $PROG_SECD bin/
+echo "Dataplane build done....."
+
+echo "---------------------------------------------------"
+echo "+                                                 +"
+echo "+            Sec Fw Mgrplane                      +"
+echo "+                                                 +"
+echo "---------------------------------------------------"
+
+PROG_CLI="bin/cli"
+PROG_SRV="bin/srv"
+
+cd ./mgrplane 
+
+rm -rf bin
+pwd
+make
+
+if [ $? -eq 0 ]; then
+	echo "Mgrplane build success!"
+else
+	echo "Error! Mgrplane build failed!"
+fi
 
 
+if [ ! -f "$PROG_CLI" ]; then
+	echo "$PROG_CLI not exist" 
+	exit
+fi
+
+if [ ! -f "$PROG_SRV" ]; then
+	echo "$PROG_SRV not exist"
+	exit
+fi
+
+cp $PROG_CLI ../bin/
+cp $PROG_SRV ../bin/
+
+echo "Mgrplane build done....."
